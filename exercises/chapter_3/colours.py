92389db6da732eb95ca35ebe5cd40ae9fa74f4a4
Luis Fernández Barba <lfernandez@plainconcepts.com>
2025-07-25 12:06:55 +0100

    Refactor folder structure

---
+++ exercises/chapter_3/colours.py
@@ -0,0 +1,8 @@
+colors = ["red", "blue", "pink", "orange"]
+print(colors)
+
+print("The first color is:", colors[0])
+
+# Access to items starting from the end using negative indexes
+print("The last color is:", colors[-1])
+print("The second to last color is:", colors[-2])
